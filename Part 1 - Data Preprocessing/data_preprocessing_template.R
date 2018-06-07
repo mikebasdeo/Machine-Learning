a13a82a753b2aa473ed2bb45ac49a0f1ddbf1f26
mikebasdeo <mikebasdeo@gmail.com>
2018-06-07 11:05:10 -0400

    Encoding categorical data

--- Part 1 - Data Preprocessing/data_preprocessing_template.R
+++ Part 1 - Data Preprocessing/data_preprocessing_template.R
@@ -6,3 +6,11 @@ dataset = read.csv("Data.csv")
 # take care of missing data
 dataset$Age = ifelse(is.na(dataset$Age), ave(dataset$Age, FUN = function(x) mean(x, na.rm =TRUE)),   dataset$Age)
 dataset$Salary = ifelse(is.na(dataset$Salary), ave(dataset$Salary, FUN = function(x) mean(x, na.rm =TRUE)),    dataset$Salary)
+
+
+# Dummy encoding for MOF
+dataset$Country = factor(dataset$Country, levels=c('France', 'Spain', 'Germany'), labels=c(1,2,3))
+
+
+# Dummy encoding for DVV
+dataset$Purchased = factor(dataset$Purchased, levels=c('Yes', 'No'), labels=c(1,0))
--- Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -17,9 +17,11 @@ x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:,3].values
 
 
-# Spyder > 3.2 has a problem displaying x, so this will help with that.
+# Spyder > 3.2 has an error re:displaying object arrays, so this will help with that.
 display_x = pd.DataFrame(x)
 display_y = pd.DataFrame(y)
+
+
 
 # Take care of missing data by taking the mean of the data available
 from sklearn.preprocessing import Imputer
@@ -28,3 +30,36 @@ imputer = Imputer(missing_values="NaN", strategy="mean", axis=0)
 imputer = imputer.fit(x[:, 1:3])
 
 x[:,1:3] = imputer.transform(x[:,1:3])
+
+# Encode categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+label_encoder_x = LabelEncoder()
+x[:, 0] = label_encoder_x.fit_transform(x[:, 0])
+
+# Dummy encoding for MOF
+one_hot_encoder = OneHotEncoder(categorical_features=[0])
+x = one_hot_encoder.fit_transform(x).toarray()
+
+# Dummy encoding for DVV
+label_encoder_y = LabelEncoder()
+y = label_encoder_y.fit_transform(y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
