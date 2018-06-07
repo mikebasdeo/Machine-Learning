ecb62fbaaa3678a4efa4b71a00aac9bba9d516ed
mikebasdeo <mikebasdeo@gmail.com>
2018-06-07 12:31:24 -0400

    preprocessing templates

--- Part 1 - Data Preprocessing/data_preprocessing_template.R
+++ Part 1 - Data Preprocessing/data_preprocessing_template.R
@@ -3,17 +3,19 @@
 # import the dataset
 dataset = read.csv("Data.csv")
 
+dataset = dataset[, 2:3]
+
 # take care of missing data
-dataset$Age = ifelse(is.na(dataset$Age), ave(dataset$Age, FUN = function(x) mean(x, na.rm =TRUE)),   dataset$Age)
-dataset$Salary = ifelse(is.na(dataset$Salary), ave(dataset$Salary, FUN = function(x) mean(x, na.rm =TRUE)),    dataset$Salary)
+# dataset$Age = ifelse(is.na(dataset$Age), ave(dataset$Age, FUN = function(x) mean(x, na.rm =TRUE)),   dataset$Age)
+# dataset$Salary = ifelse(is.na(dataset$Salary), ave(dataset$Salary, FUN = function(x) mean(x, na.rm =TRUE)),    dataset$Salary)
 
 
-# Dummy encoding for MOF
-dataset$Country = factor(dataset$Country, levels=c('France', 'Spain', 'Germany'), labels=c(1,2,3))
-
-
-# Dummy encoding for DVV
-dataset$Purchased = factor(dataset$Purchased, levels=c('Yes', 'No'), labels=c(1,0))
+# # Dummy encoding for MOF
+# dataset$Country = factor(dataset$Country, levels=c('France', 'Spain', 'Germany'), labels=c(1,2,3))
+# 
+# 
+# # Dummy encoding for DVV
+# dataset$Purchased = factor(dataset$Purchased, levels=c('Yes', 'No'), labels=c(1,0))
 
 # Split data into training set and test set
 library(caTools)
@@ -24,6 +26,6 @@ training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
 # Feature Scaling
-training_set[,2:3] = scale(training_set[,2:3])
-test_set[,2:3] = scale(test_set[,2:3])
+# training_set[,2:3] = scale(training_set[,2:3])
+# test_set[,2:3] = scale(test_set[,2:3])
 
--- Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -5,9 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
 dataset = pd.read_csv("Data.csv")
-
 # Distinguish between matrix of features and dependent variable vector
 
 # Matrix of features, all rows, columns except last one.
@@ -16,33 +14,29 @@ x = dataset.iloc[:, :-1].values
 # Dependent variable vector
 y = dataset.iloc[:,3].values
 
-
 # Spyder > 3.2 has an error re:displaying object arrays, so this will help with that.
 display_x = pd.DataFrame(x)
 display_y = pd.DataFrame(y)
 
 
-
 # Take care of missing data by taking the mean of the data available
-from sklearn.preprocessing import Imputer
-
-imputer = Imputer(missing_values="NaN", strategy="mean", axis=0)
-imputer = imputer.fit(x[:, 1:3])
-
-x[:,1:3] = imputer.transform(x[:,1:3])
+#from sklearn.preprocessing import Imputer
+#imputer = Imputer(missing_values="NaN", strategy="mean", axis=0)
+#imputer = imputer.fit(x[:, 1:3])
+#x[:,1:3] = imputer.transform(x[:,1:3])
 
 # Encode categorical data
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-label_encoder_x = LabelEncoder()
-x[:, 0] = label_encoder_x.fit_transform(x[:, 0])
-
-# Dummy encoding for MOF
-one_hot_encoder = OneHotEncoder(categorical_features=[0])
-x = one_hot_encoder.fit_transform(x).toarray()
-
-# Dummy encoding for DVV
-label_encoder_y = LabelEncoder()
-y = label_encoder_y.fit_transform(y)
+#from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+#label_encoder_x = LabelEncoder()
+#x[:, 0] = label_encoder_x.fit_transform(x[:, 0])
+#
+## Dummy encoding for MOF
+#one_hot_encoder = OneHotEncoder(categorical_features=[0])
+#x = one_hot_encoder.fit_transform(x).toarray()
+#
+## Dummy encoding for DVV
+#label_encoder_y = LabelEncoder()
+#y = label_encoder_y.fit_transform(y)
 
 
 # Split data into training set and test set
@@ -50,11 +44,11 @@ from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2, random_state=0)
 
 # Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc_x = StandardScaler()
-# fit to the training, data, not to the test data.
-x_train = sc_x.fit_transform(x_train)
-x_test = sc_x.transform(x_test)
+#from sklearn.preprocessing import StandardScaler
+#sc_x = StandardScaler()
+## fit to the training, data, not to the test data.
+#x_train = sc_x.fit_transform(x_train)
+#x_test = sc_x.transform(x_test)
 
 
 
