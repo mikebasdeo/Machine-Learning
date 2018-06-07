7dfc8e54f98a25853da536c99c3316cf6de4db6c
mikebasdeo <mikebasdeo@gmail.com>
2018-06-07 11:23:21 -0400

    Split dataset into training and test sets

--- Part 1 - Data Preprocessing/data_preprocessing_template.R
+++ Part 1 - Data Preprocessing/data_preprocessing_template.R
@@ -14,3 +14,12 @@ dataset$Country = factor(dataset$Country, levels=c('France', 'Spain', 'Germany')
 
 # Dummy encoding for DVV
 dataset$Purchased = factor(dataset$Purchased, levels=c('Yes', 'No'), labels=c(1,0))
+
+# Split data into training set and test set
+library(caTools)
+set.seed(123)
+
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
--- Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -45,7 +45,9 @@ label_encoder_y = LabelEncoder()
 y = label_encoder_y.fit_transform(y)
 
 
-
+# Split data into training set and test set
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2, random_state=0)
 
 
 
